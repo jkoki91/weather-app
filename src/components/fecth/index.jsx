e8e105c33207a809c75586ac152ec7b7719fc825
DESKTOP-1U4HJ0I\jorge <jkokeito@gmail.com>
2022-03-09 10:34:31 +0100

    el fetch sin la key

--- src/components/fecth/index.jsx
+++ src/components/fecth/index.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 function WheatherInfo() {
     let [data, updateData] = useState([])
     useEffect(() => {
-        fetch('https://api.openweathermap.org/data/2.5/weather?q=Cordoba,ES&APPID=1f7689b2591acb5efd5d91b7e124bf44')
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=Cordoba,ES&APPID=')
           .then(j => j.json())
           .then(r => updateData(r))
     }, [])
