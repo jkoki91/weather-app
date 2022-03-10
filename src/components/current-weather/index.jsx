3d0261a072bd78a1b120d61e9d795c51909ced3a
ann <asog.jrti@gmail.com>
2022-03-10 17:40:41 +0100

    some hooks and contexts

---
+++ src/components/current-weather/index.jsx
@@ -0,0 +1,20 @@
+import { coordsCity } from "../fecth";
+import React from "react";
+import { useEffect, useState } from "react";
+import { useFetchWithCoords, useFetchWithCity } from "../../custom-hook/useFetch";
+
+function CurrentWeather() {
+    console.log('holaaa');
+    useFetchWithCoords();
+    // useFetchWithCity();
+
+    return (
+        <React.Fragment>
+            <h3>Temperatura: </h3>
+            <h3>Sensación térmica: </h3>
+            <h3>Índice UV: </h3>
+        </React.Fragment>
+    )
+}
+
+export default CurrentWeather;
--- src/components/fecth/index.jsx
+++ src/components/fecth/index.jsx
@@ -1,15 +1,42 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { CoordsContext } from "../../context/coords-context";
+
 
 function WheatherInfo() {
+
+
     let [data, updateData] = useState([])
     useEffect(() => {
         fetch('https://api.openweathermap.org/data/2.5/weather?q=Cordoba,ES&APPID=')
-          .then(j => j.json())
-          .then(r => updateData(r))
+            .then(j => j.json())
+            .then(r => updateData(r))
     }, [])
     console.log(data)
-    return(
+    return (
         <p>{JSON.stringify(data)}</p>
     )
 }
 export default WheatherInfo;
+
+export const useCoords = async () => {
+    const [city, setCity, latitude, setLatitude, longitude, setLongitude] = useContext(CoordsContext);
+    const API_KEY = 'fd55d838139cc3180f89c425bfeb6eb4';
+    const apiCity = `http://api.openweathermap.org/geo/1.0/direct?q=${city},&limit=2&appid=${API_KEY}`
+
+    const res = await fetch(apiCity)
+    const data = await res.json()
+
+    console.log(data);
+    const info = Object.entries(data[0]);
+    console.log(info);
+    const [, latCoord] = info[2];
+    const [, lonCoord] = info[3];
+    setLatitude(latCoord);
+    setLongitude(lonCoord);
+
+
+    // const { lat, lon } = info;
+    // console.log(lat, lon);
+    // console.log(Object.entries(data.find(d => d.country === 'ES')));
+
+}
