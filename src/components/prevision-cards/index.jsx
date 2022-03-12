fff4c810e2207f121f0c27dde0cb9406b865fbb3
ann <asog.jrti@gmail.com>
2022-03-12 19:52:30 +0100

    print data on prevision cards

--- src/components/prevision-cards/index.jsx
+++ src/components/prevision-cards/index.jsx
@@ -1,31 +1,32 @@
 import { Container, Row, Col } from "react-bootstrap";
 import WeatherIcon from "../weather-icon";
 import tempIcon from "../../assets/weather-icons/temp.svg";
+import rainIcon from '../../assets/weather-icons/rain.svg';
 import "./style.css";
 
 
-export default function PrevisionCards() {
+export default function PrevisionCards({weekday, day, degrees, rain}) {
   return (
-    <section className="prev-card__container">
+    <article className="prev-card__container">
       <div className="prev-card__weather-icon">
         <WeatherIcon className="prev-card__wi" type="01d"></WeatherIcon>
       </div>
 
       <div className="prev-card__info">
         <div>
-          <h3 className="prev-card__date">Tuesday, 14 March</h3>
+          <h3 className="prev-card__date">{weekday}, {day} March</h3>
         </div>
         <div className="prev-card__temp-rain">
           <div className="prev-card_temp prev-info">
             <img src={tempIcon} />
-            <p>12ºC</p>
+            <p>{degrees}ºC</p>
           </div>
           <div className="prev-card_rain prev-info">
-            <img src={tempIcon} />
-            <p>10%</p>
+            <img src={rainIcon} />
+            <p>{rain}%</p>
           </div>
         </div>
       </div>
-    </section>
+    </article>
   );
 }
